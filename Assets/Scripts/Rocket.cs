bfde88941295010d2ae1065d0c0b150791e4b06d
Atin Agnihotri <atinagnihotri@gmail.com>
2019-12-14 20:01:41 +0530

    Greyboxing Level 1 and 2 and setuping up a level loop

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    // TODO FIX LGT BUG
+
     /// <summary>
     /// 
     /// </summary>
@@ -16,6 +19,11 @@ public class Rocket : MonoBehaviour
     private AudioSource rocketAS;
     [SerializeField]
     private const float maxThrust = 50f;
+    
+    enum State { Alive, Dead, Transcending };
+    [SerializeField]
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,8 +34,12 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+        
     }
 
     private void Rotate()
@@ -35,7 +47,6 @@ public class Rocket : MonoBehaviour
         /// <summary>
         /// 
         /// </summary>
-
         rocketRB.freezeRotation = true; // Rotation w/o affecting Phyics
         float rotMultiplier = rotThrust * Time.deltaTime;
 
@@ -48,7 +59,8 @@ public class Rocket : MonoBehaviour
             transform.Rotate(-Vector3.forward * rotMultiplier);
         }
 
-        rocketRB.freezeRotation = false; // Rotation control back to Phyics
+        //rocketRB.freezeRotation = false; // Rotation control back to Phyics [OLD]
+        rocketRB.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
     }
 
     private void Thrust()
@@ -79,5 +91,43 @@ public class Rocket : MonoBehaviour
             //if (thrust != 0f)
             //    thrust = 0f;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive)
+            return;
+
+        switch (collision.gameObject.tag)
+        {
+            case "Safe":
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextScene", 1f);  // parameterise time
+                break;
+            case "Fatal":
+                state = State.Dead;
+                rocketAS.Stop();
+                Invoke("LoadFirstScene", 1f);
+                break;
+            case "Fuel":
+                print("Refueled!");
+                break;
+            default:
+                //print("Collided with Other");
+                break;
+        }
+    }
+
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
